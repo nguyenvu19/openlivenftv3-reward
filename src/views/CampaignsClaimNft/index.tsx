0b9194b9315e607df88c93bdfc2d12300bd408eb
DavidNgo <ngohung2395@gmail.com>
2022-10-14 21:17:01 +0700

    update: claim function

--- src/views/CampaignsClaimNft/index.tsx
+++ src/views/CampaignsClaimNft/index.tsx
@@ -2,13 +2,14 @@ import Container from 'components/Layout/Container'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import { useContractCampaigns } from 'hooks/useContract'
 import { useCampaignItem, usePollCoreCampaignsData } from 'state/campaigns/hooks'
-import { Router, useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import { useMyNftsList } from 'state/nfts/hooks'
 import styled from 'styled-components'
 import { NftType } from 'state/nfts/types'
 import { CAMPAIGN_STATUS } from 'state/campaigns/types'
 import { useToast } from '@pancakeswap/uikit'
 import { useTransactionAdder } from 'state/transactions/hooks'
+import moment from 'moment'
 import ClaimNftList from './components/ClaimNftList'
 
 const WCampaignsClaimNft = styled.div`
@@ -42,7 +43,21 @@ const CampaignsClaimNft: React.FC<React.PropsWithChildren> = () => {
       return toastError('Campaign is ended')
     }
 
+    if (campaign.currentPool >= campaign.totalPool) {
+      if (cb) cb()
+      return toastError('Sold out')
+    }
+
     try {
+      const lastTimeClaim = await (await contractCampaign.claimTimes(nftItem.token_id)).toNumber()
+      if (lastTimeClaim && lastTimeClaim > 0) {
+        const currentTime = new Date(moment(new Date()).format('YYYY/MM/DD')).getTime()
+        if (lastTimeClaim * 1000 >= currentTime) {
+          if (cb) cb()
+          return toastError(`NFT ID: ${nftItem.token_id}, Claimed today`)
+        }
+      }
+
       const response = await contractCampaign.claimOPV([nftItem.token_id], campaignId?.[0])
       addTransaction(response, {
         summary: `Claim reward by NFT: ${nftItem.token_id}`,
