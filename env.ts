6cf99b9421d0ab188231c21d0bd375bd17c26be3
DavidNgo <ngohung2395@gmail.com>
2022-10-21 17:38:28 +0700

    update

--- env.ts
+++ env.ts
@@ -20,12 +20,34 @@ const ENV_DEVELOPMENT = {
   NEXT_PUBLIC_GTAG: 'GTM-PXLD3XW',
 }
 
+// staging
+const ENV_STAGING = {
+  NEXT_PUBLIC_NODE_ENV: 'staging',
+  NEXT_PUBLIC_APP_URL: 'https://app-openlivenft-uat.netlify.com',
+  NEXT_PUBLIC_APP_USER_URL: 'https://openlivev3-uat.netlify.app',
+  NEXT_PUBLIC_APP_USER_API: 'https://api-nft.openlive.finance/api/v1',
+
+  NEXT_PUBLIC_DEFAULT_CHAIN_ID: 56,
+  NEXT_PUBLIC_TOKEN_ADDRESS: '',
+  NEXT_PUBLIC_CONTRACT_ADDRESS: '',
+  NEXT_PUBLIC_NFT_ADDRESS: '',
+  NEXT_PUBLIC_CONTRACT_FARM: '',
+  NEXT_PUBLIC_CONTRACT_STAKING: '',
+
+  NEXT_PUBLIC_MORALIS_API: 'https://deep-index.moralis.io/api/v2',
+  NEXT_PUBLIC_MORALIS_API_KEY: '3EwdfGDMjcl235uf6DZIniUEIY6rZmbnY6tAWLDeSMFm2r4BW0QgyEG1NelJJNLZ',
+  NEXT_PUBLIC_MORALIS_DAPP_URL: 'https://zh59rm5t5l4j.usemoralis.com:2053/server',
+  NEXT_PUBLIC_MORALIS_DAPP_ID: 'W0kUA0Xr59ZG6M03m4jZj8BfE89qwMIgLiaxldk9',
+
+  NEXT_PUBLIC_GTAG: 'GTM-PXLD3XW',
+}
+
 // Production
 const ENV_PRODUCTION = {
   NEXT_PUBLIC_NODE_ENV: 'production',
-  NEXT_PUBLIC_APP_URL: 'https://app-openlivenft-dev.netlify.com',
-  NEXT_PUBLIC_APP_USER_URL: 'https://openlivev3-dev.netlify.app',
-  NEXT_PUBLIC_APP_USER_API: 'https://api-nft-dev.openlive.finance/api/v1',
+  NEXT_PUBLIC_APP_URL: 'https://app-openlivenft-uat.netlify.com',
+  NEXT_PUBLIC_APP_USER_URL: 'https://openlivev3-uat.netlify.app',
+  NEXT_PUBLIC_APP_USER_API: 'https://api-nft.openlive.finance/api/v1',
 
   NEXT_PUBLIC_DEFAULT_CHAIN_ID: 56,
   NEXT_PUBLIC_TOKEN_ADDRESS: '',
@@ -43,4 +65,4 @@ const ENV_PRODUCTION = {
 }
 
 const nodeEnv = process.env.NEXT_PUBLIC_NODE_ENV || 'production'
-export default nodeEnv === 'development' ? ENV_DEVELOPMENT : ENV_PRODUCTION
+export default nodeEnv === 'development' ? ENV_DEVELOPMENT : nodeEnv === 'staging' ? ENV_STAGING : ENV_PRODUCTION
