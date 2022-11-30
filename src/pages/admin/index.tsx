9a02916bc85d4253724dcfe258c57bf785c04084
dilysngo <ngohung2393@gmail.com>
2022-11-30 08:55:45 +0700

    config: admin page

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,8 @@
+import AdminHomePage from 'views/admin/Home'
+
+const Admin = () => {
+  return <AdminHomePage />
+}
+
+Admin.mp = true
+export default Admin
---
+++ src/views/admin/Home/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const WHome = styled.div`
+  padding-bottom: 100px;
+`
+
+const Home: React.FC = () => {
+  return <WHome>Home admin</WHome>
+}
+
+export default Home
